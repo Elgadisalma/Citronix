442c4b8760e8051fe826950f5575afcc27ede1ec
Elgadisalma <elgadi.salma0@gmail.com>
2024-11-17 15:21:20 +0100

    CIT-3 connexion avec la base de donnee

--- Citronix/src/main/java/com/example/Citronix/CitronixApplication.java
+++ Citronix/src/main/java/com/example/Citronix/CitronixApplication.java
@@ -1,4 +1,4 @@
-package com.example.Citronix;
+package com.example.citronix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Citronix/src/test/java/com/example/Citronix/CitronixApplicationTests.java
+++ Citronix/src/test/java/com/example/Citronix/CitronixApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Citronix;
+package com.example.citronix;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
